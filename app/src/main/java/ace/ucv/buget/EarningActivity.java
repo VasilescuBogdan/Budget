33cd7133cc45cae05fc110afd6c2848e8e693388
Teodor Bogdan Vasilescu <vasilescubogdan1@gmail.com>
2023-03-12 16:43:27 +0200

    UI

---
+++ app/src/main/java/ace/ucv/buget/EarningActivity.java
@@ -0,0 +1,14 @@
+package ace.ucv.buget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EarningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earning);
+    }
+}
---
+++ app/src/main/java/ace/ucv/buget/ExpenseActivity.java
@@ -0,0 +1,13 @@
+package ace.ucv.buget;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense);
+    }
+}
